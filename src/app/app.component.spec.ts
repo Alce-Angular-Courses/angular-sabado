5016eeb71121db87ee7a423177a6523f7ad763ec
alce65 <alce65@hotmail.es>
2020-06-06 12:24:09 +0200

    Proyecto inicial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,19 +17,18 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    expect(app).toBeTruthy();  });
 
-  it(`should have as title 'angular-sabado'`, () => {
+  it(`should have as title 'Angular Sabados en CAS'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-sabado');
+    expect(app.title).toEqual('Angular Sabados en CAS');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-sabado app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-sabado';
+  title = 'Angular Sabados en CAS';
+  logo = '../assets/logo_angular.svg';
 }
